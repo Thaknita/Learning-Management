1778ba2ecad6ac7044efad7dbdc116355e612cac
Mao_Thaknita <thaknita555@gmail.com>
2024-03-06 15:24:26 +0700

    configure shell script

---
+++ boot.sh
@@ -0,0 +1,3 @@
+docker rmi thaknita/e-learning-api:1.0 &&
+docker compose up -d &&
+docker logs e-learning-api -f
---
+++ build.sh
@@ -0,0 +1,3 @@
+./gradlew clean build &&
+docker compose build &&
+docker push thaknita/e-learning-api:1.0
